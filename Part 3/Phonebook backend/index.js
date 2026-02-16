0c4f50e7d97ecb9e5ec5d8a55e057d17c8b50c46
Mike Taslakjian <taslakjianmike@gmail.com>
2026-02-17 00:10:53 +0400

    Completed Phonebook backend step 5

--- Part 3/Phonebook backend/index.js
+++ Part 3/Phonebook backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
     { 
@@ -53,6 +54,27 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const id = Math.floor(Math.random() * 1000000 + 1000000);
+  const body = request.body;
+
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: "name/number missing"
+    });
+  }
+
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  };
+
+  persons = persons.concat(person);
+  console.log(request.headers);
+  response.json(person);
 });
 
 const PORT = 3001;
