26eeeb9af925e995aa0b6fbf3d45560d369c0cb7
Mike Taslakjian <taslakjianmike@gmail.com>
2026-02-18 19:03:31 +0400

    Completed Full Stack Phonebook

--- Part 3/Phonebook backend/index.js
+++ Part 3/Phonebook backend/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const app = express();
-
-app.use(express.json());
-app.use(cors());
 
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
 });
 
+app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+app.use(express.static("dist"));
 
 let persons = [
     { 
