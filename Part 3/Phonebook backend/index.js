71337ad92ccab71d726b11636f81a95185d1a0c4
Mike Taslakjian <taslakjianmike@gmail.com>
2026-02-24 18:37:52 +0400

    Completed Phonebook database, part 1

--- Part 3/Phonebook backend/index.js
+++ Part 3/Phonebook backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person.js");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -11,31 +13,10 @@ app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 app.use(express.static("dist"));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person
+      .find({})
+      .then((people) => response.json(people));
 });
 
 app.get("/info", (request, response) => {
