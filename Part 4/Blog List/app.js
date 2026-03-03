e51af0977143da2771b26c2d4ab54af1e2843512
Mike Taslakjian <taslakjianmike@gmail.com>
2026-03-03 15:38:51 +0400

    Completed Blog List, step 2

---
+++ Part 4/Blog List/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config.js')
+const logger = require('./utils/logger.js')
+const blogsRouter = require("./controllers/blogs.js");
+
+const app = express();
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
